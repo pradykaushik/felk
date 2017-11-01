7dafa1a3fcf1da19efb0721ce95e2be3ea006158
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-10-31 20:52:00 -0400

    trying out the JSON marshalling using Jackson library. Reading JSON input file and converting it into corresponding objects.

--- src/main/java/com/sunybingcloud/felk/demo/SampleScheduler.java
+++ src/main/java/com/sunybingcloud/felk/demo/SampleScheduler.java
@@ -1,5 +1,20 @@
+package com.sunybingcloud.felk.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sunybingcloud.felk.config.Schema;
+
+import java.io.File;
+import java.io.IOException;
+
 public class SampleScheduler {
   public static void main(String[] args) {
-    System.out.println("Welcome to Felk!");
+    File workloadFile = new File("sample_workload.json");
+    // Reading JSON from workloadFile and converting it to type com.sunybingcloud.felk.config.Schema.
+    try {
+      Schema schema = new ObjectMapper().readValue(workloadFile, Schema.class);
+      System.out.println(schema);
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
   }
 }
