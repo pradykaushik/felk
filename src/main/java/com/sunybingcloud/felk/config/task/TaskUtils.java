9e3dbc4e017faeae646fbf665f097fbf05c113dc
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-11-02 19:43:13 -0400

    a naive utility to create a unique task identifier that is unique across all tasks and all instances of the same task.

--- src/main/java/com/sunybingcloud/felk/config/task/TaskUtils.java
+++ src/main/java/com/sunybingcloud/felk/config/task/TaskUtils.java
@@ -1,3 +1,15 @@
 package com.sunybingcloud.felk.config.task;
 
-public final class TaskUtils {}
+public final class TaskUtils {
+  /**
+   * Create a task identifier that is unique across tasks,
+   *  and across all the instances of the same task.
+   * @param taskName name of the task specified in the input schema.
+   * @param instance instance to which this task corresponds to.
+   * @return a unique identifier for the task.
+   */
+  static String createTaskID(String taskName, int instance) {
+    // This is a very naive way of creating identifiers.
+    return taskName + "-" + instance;
+  }
+}
