13d346ac7092e65810f64283a83091b86d69fc84
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-11-03 15:42:45 -0400

    separated driver code from the framework code. Updated build.gradle to be consistent with this change. Added shutdown() function to the FelkFramework

---
+++ src/main/java/com/sunybingcloud/felk/FelkFramework.java
@@ -0,0 +1,134 @@
+package com.sunybingcloud.felk;
+
+import com.netflix.fenzo.TaskScheduler;
+import com.netflix.fenzo.VMTaskFitnessCalculator;
+import com.netflix.fenzo.VirtualMachineLease;
+import com.netflix.fenzo.functions.Action1;
+import com.netflix.fenzo.plugins.BinPackingFitnessCalculators;
+import com.sunybingcloud.felk.config.Schema;
+import com.sunybingcloud.felk.config.task.Task;
+import com.sunybingcloud.felk.sched.FelkScheduler;
+import org.apache.mesos.MesosSchedulerDriver;
+import org.apache.mesos.Protos;
+import org.apache.mesos.Scheduler;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class FelkFramework {
+
+    private Collection<Task> taskQueue;
+    private TaskScheduler taskScheduler;
+    private MesosSchedulerDriver mesosSchedulerDriver;
+    private String mesosMaster;
+    private final AtomicReference<MesosSchedulerDriver> mesosSchedulerDriverRef = new AtomicReference<>();
+    private Scheduler felkScheduler;
+    private AtomicBoolean isShutdown = new AtomicBoolean(false);
+
+    public class LeaseRejectActionBuilder {
+        private Action1<VirtualMachineLease> leaseRejectAction;
+        private Map<String, Action1<VirtualMachineLease>> leaseRejectActionsDirectory = createLeaseRejectActions();
+
+        /**
+         * Defining different leaseRejectActions associated with a name. This is done in order for the user
+         * to be able to specify the lease reject action in the configuration file.
+         * @return a mapping from a name to a lease reject action.
+         */
+        private Map<String, Action1<VirtualMachineLease>> createLeaseRejectActions() {
+            Map<String, Action1<VirtualMachineLease>> actions = new HashMap<>();
+            actions.put("decline", virtualMachineLease ->
+                    mesosSchedulerDriverRef.get().declineOffer(virtualMachineLease.getOffer().getId()));
+            return actions;
+        }
+
+        public LeaseRejectActionBuilder withLeaseRejectAction(String actionName) {
+            leaseRejectAction = leaseRejectActionsDirectory.get(actionName);
+            return this;
+        }
+
+        public Action1<VirtualMachineLease> build() {
+            return leaseRejectAction;
+        }
+    }
+
+    public final class Builder {
+        private Map<String, VMTaskFitnessCalculator> fitnessCalculators = createFitnessCalculatorRegistry();
+        private TaskScheduler taskScheduler;
+        private TaskScheduler.Builder taskSchedulerBuilder = new TaskScheduler.Builder();
+        private Collection<Task> taskQueue;
+
+        private Map<String, VMTaskFitnessCalculator> createFitnessCalculatorRegistry() {
+            Map<String, VMTaskFitnessCalculator> fcr = new HashMap<>();
+            fcr.put("cpuBinPacker", BinPackingFitnessCalculators.cpuBinPacker);
+            fcr.put("memBinPacker", BinPackingFitnessCalculators.memoryBinPacker);
+            fcr.put("cpuMemBinPacker", BinPackingFitnessCalculators.cpuMemBinPacker);
+            return fcr;
+        }
+
+        /**
+         * Build the taskScheduler based on the schema.
+         * @param schema The workload configuration schema.
+         * @return Builder
+         */
+        public Builder withSchema(Schema schema) {
+            // Input validation needs to have been done prior to this.
+            taskQueue = schema.getTasks();
+            // If no fitness calculator specified, then we're not going to be using one.
+            String fc;
+            if ((fc = schema.getFitnessCalculator()) != null) {
+                taskSchedulerBuilder.withFitnessCalculator(fitnessCalculators.get(fc));
+            }
+            // If lease offer expiry not specified (or is 0), then not explicitly specifying (using default 120 seconds).
+            if (schema.getLeaseOfferExpirySeconds() > 0) {
+                taskSchedulerBuilder.withLeaseOfferExpirySecs(schema.getLeaseOfferExpirySeconds());
+            }
+            if (schema.isDisableShortfallEvaluation()) taskSchedulerBuilder.disableShortfallEvaluation();
+
+            // Default lease reject action is 'decline'.
+            // Right now, we have only one kind of action that can be specified.
+            // Modify code when we have non-default lease reject actions specifiable.
+            taskSchedulerBuilder.withLeaseRejectAction(new LeaseRejectActionBuilder()
+                    .withLeaseRejectAction(schema.getLeaseRejectAction()).build());
+            return this;
+        }
+
+        public FelkFramework build() {
+            return new FelkFramework(this);
+        }
+    }
+
+    public FelkFramework() {}
+
+    public FelkFramework(Builder builder) {
+        taskQueue = builder.taskQueue;
+        taskScheduler = builder.taskSchedulerBuilder.build();
+        felkScheduler = new FelkScheduler(taskScheduler);
+    }
+
+    public FelkFramework buildSchedulerDriver(String mesosMaster) {
+        // Creating FrameworkInfo
+        Protos.FrameworkInfo frameworkInfo = Protos.FrameworkInfo.newBuilder()
+                .setName("felk")
+                .setUser("")
+                .build();
+        mesosSchedulerDriver = new MesosSchedulerDriver(felkScheduler, frameworkInfo, mesosMaster);
+        mesosSchedulerDriverRef.set(mesosSchedulerDriver);
+        return this;
+    }
+
+    public void execute() {
+        // Need to launch felk to execute all the tasks using Fenzo's task scheduler.
+        System.out.println("Executing...");
+    }
+
+    public void shutdown() {
+        System.out.println("Shutting down mesos scheduler driver...");
+        mesosSchedulerDriver.stop();
+        // Letting the runner know that the driver has been shutdown.
+        // This trigger can be used to perform any necessary clean up before Felk stops running.
+        isShutdown.set(true);
+    }
+}
---
+++ src/main/java/com/sunybingcloud/felk/FrameworkDriver.java
@@ -0,0 +1,75 @@
+package com.sunybingcloud.felk;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sunybingcloud.felk.cli.CLIBuilder;
+import com.sunybingcloud.felk.cli.UsageException;
+import com.sunybingcloud.felk.config.Schema;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FrameworkDriver {
+
+  public static void main(String[] args) {
+    CommandLineParser commandLineParser = new DefaultParser();
+    CommandLine commandLine;
+    // Parsing command line arguments
+    if (args.length == 0 ) {
+      throw new UsageException();
+    } else {
+      try {
+        commandLine = commandLineParser.parse(CLIBuilder.options, args);
+      } catch (ParseException pe) {
+        pe.printStackTrace();
+        throw new UsageException(pe.getMessage());
+      }
+    }
+    // Printing possible command line options and exiting.
+    if (commandLine.hasOption("help")) {
+      HelpFormatter helpFormatter = new HelpFormatter();
+      helpFormatter.printHelp("java -jar build/libs/felk-1.0-SNAPSHOT.jar [-h] --config FILE --master MASTER_LOCATION", CLIBuilder.options);
+      System.exit(0);
+    }
+
+    String configurationFilename = commandLine.getOptionValue("config");
+    String masterLocation = commandLine.getOptionValue("master");
+
+    Schema schema = null;
+    if (configurationFilename != null) {
+      File workloadFile = new File(configurationFilename);
+      // Reading JSON from workloadFile and converting it to type com.sunybingcloud.felk.config.Schema.
+      try {
+        schema = new ObjectMapper().readValue(workloadFile, Schema.class);
+        System.out.println(schema);
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+    }
+
+    if (masterLocation != null) {
+      System.out.println("Master location: " + masterLocation);
+    }
+
+    if (schema == null) {
+      System.out.println("Something went wrong!!");
+      System.exit(0);
+    }
+
+    // Launching
+    // Issue: Unnecessary duplicate object of FelkFramework created in the beginning.
+    //  This was a workaround to solve the issue of the non-static inner classes.
+    // TODO: Fix this.
+    FelkFramework framework = new FelkFramework().new Builder()
+            .withSchema(schema)
+            .build();
+
+    framework.buildSchedulerDriver(masterLocation).execute();
+    // Shutting down the framework
+    framework.shutdown();
+  }
+}
