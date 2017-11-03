9d49fafa1bcb036646dfa0fd7a0695ec211a2a8a
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-11-03 19:24:35 -0400

    Abstracted out the common behavior for all Felk schedulers. Now an implementer needs to be only implement resourceOffers().

--- src/main/java/com/sunybingcloud/felk/sched/FelkSchedulerImpl.java
+++ src/main/java/com/sunybingcloud/felk/sched/FelkSchedulerImpl.java
@@ -1,80 +1,33 @@
 package com.sunybingcloud.felk.sched;
 
 import com.netflix.fenzo.TaskScheduler;
+import com.netflix.fenzo.plugins.VMLeaseObject;
 import org.apache.mesos.Protos;
-import org.apache.mesos.Scheduler;
 import org.apache.mesos.SchedulerDriver;
 
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class FelkScheduler implements Scheduler {
+/**
+ * A basic implementation of the {@link FelkScheduler}.
+ */
+public class FelkSchedulerImpl extends FelkScheduler {
 
-  private TaskScheduler taskScheduler;
-
-  public FelkScheduler(TaskScheduler taskScheduler) {
+  public FelkSchedulerImpl(TaskScheduler taskScheduler) {
     this.taskScheduler = taskScheduler;
+    this.leaseQueue = new LinkedBlockingQueue<>();
   }
 
   /**
-   * Any previous resource offers are invalid. Fenzo scheduler needs to expire all leases.
-   */
-  @Override
-  public void registered(SchedulerDriver driver, Protos.FrameworkID frameworkId, Protos.MasterInfo masterInfo) {
-    System.out.println("Felk registered!");
-    taskScheduler.expireAllLeases();
-  }
-
-  /**
-   * Similar to {@code registered()}, Fenzo's scheduler needs to expire all leases.
-   */
-  @Override
-  public void reregistered(SchedulerDriver driver, Protos.MasterInfo masterInfo) {
-    System.out.println("Felk reregistered!");
-    taskScheduler.expireAllLeases();
-  }
-
-  /**
-   * 
-   * @param driver
-   * @param offers
+   * Adding the resource offers to the leaseQueue Fenzo's task scheduler can pick it
+   * up when it's assigning tasks to hosts corresponding to offers.
    */
   @Override
   public void resourceOffers(SchedulerDriver driver, List<Protos.Offer> offers) {
-
-  }
-
-  @Override
-  public void offerRescinded(SchedulerDriver driver, Protos.OfferID offerId) {
-
-  }
-
-  @Override
-  public void statusUpdate(SchedulerDriver driver, Protos.TaskStatus status) {
-
-  }
-
-  @Override
-  public void frameworkMessage(SchedulerDriver driver, Protos.ExecutorID executorId, Protos.SlaveID slaveId, byte[] data) {
-
-  }
-
-  @Override
-  public void disconnected(SchedulerDriver driver) {
-
-  }
-
-  @Override
-  public void slaveLost(SchedulerDriver driver, Protos.SlaveID slaveId) {
-
-  }
-
-  @Override
-  public void executorLost(SchedulerDriver driver, Protos.ExecutorID executorId, Protos.SlaveID slaveId, int status) {
-
-  }
-
-  @Override
-  public void error(SchedulerDriver driver, String message) {
-
+    for (Protos.Offer offer : offers) {
+      System.out.println("Received Offer [" + offer.getId().getValue() + "]" +
+              " for host [" + offer.getHostname() + "]");
+      leaseQueue.offer(new VMLeaseObject(offer));
+    }
   }
 }
